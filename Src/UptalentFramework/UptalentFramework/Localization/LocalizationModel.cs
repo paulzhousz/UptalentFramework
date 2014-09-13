922bcb4cf3bedc99033403a5e27f75a1d61eb890
paulzhousz <paulzhousz@gmail.com>
2014-09-13 10:03:11 +0800

    w

---
+++ Src/UptalentFramework/UptalentFramework/Localization/LocalizationModel.cs
@@ -0,0 +1,13 @@
+﻿using PetaPoco;
+
+namespace UptalentFramework.Localization
+{
+    public partial class LocalizationModel
+    {
+        public string Key { get; set; }
+
+        public string Culture { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Src/UptalentFramework/UptalentFramework/MVC/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using UptalentFramework.Localization;
+
+namespace UptalentFramework.MVC
+{
+    public abstract class BaseController:Controller
+    {
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
+        {
+            // invoke the localization logic
+            LocalizationControllerHelper.OnBeginExecuteCore(this);
+
+            // invoke the base method
+            return base.BeginExecuteCore(callback, state);
+        }
+    }
+}
